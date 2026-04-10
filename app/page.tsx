ae62ab37240c9d1d12e7e3a3047aace7efad442b
Ali-javed123 <muhammadaliadil999@gmail.com>
2026-04-10 07:44:29 +0500

    SD

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { HomeCards } from '../components/HomeCards';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-950 dark:to-slate-900 p-6 flex items-center justify-center">
+    <main className="min-h-screen  p-6 flex items-center justify-center">
       <HomeCards />
     </main>
   );
